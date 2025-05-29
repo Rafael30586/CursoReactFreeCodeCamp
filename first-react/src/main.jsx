410335a52d08240b622498ecc4de97a6a8050d42
Rafael305 <fraq86@gmail.com>
2025-05-29 17:07:52 -0300

    primer custom component

--- first-react/src/main.jsx
+++ first-react/src/main.jsx
@@ -1,11 +1,15 @@
-import { StrictMode } from 'react'
+//import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
-import { createElement } from 'react'
+//import App from './App.jsx'
+//import { createElement } from 'react'
 
 const root = createRoot(document.getElementById("root"));
-const reactElement = createElement("h1",null,"Hello from createElement")
+//const reactElement = createElement("h1",null,"Hello from createElement")
+//const reactElement = <h1><span>Hello from JSX</span></h1>
+const reactElement = <h1 id='hello'>Hello from JSX</h1>
+
+console.log(reactElement)
 
 root.render(/*
   <StrictMode>
