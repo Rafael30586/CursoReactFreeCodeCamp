7b3d1e55f9a12b0f67a43f1f84c268744f375142
Rafael305 <fraq86@gmail.com>
2025-06-05 17:09:06 -0300

    travel journal

--- fifth-react/src/components/Navbar.jsx
+++ fifth-react/src/components/Navbar.jsx
@@ -1,5 +1,9 @@
 export default function Navbar(){
     return(
-        <h1>NavBar goes here</h1>
+        <header className="header">
+            <nav>
+                <img className="react-logo" src="src/assets/react-logo.png" alt="React logo" /> <span>React facts</span>
+            </nav>
+        </header>
     )
 }
---
+++ travel-journal/src/components/Header.jsx
@@ -0,0 +1,5 @@
+export default function Header(){
+    return(
+        <header><img src="/src/assets/blue-globe.png" alt="globe icon" /><span>My travel journal</span></header>
+    )
+}
