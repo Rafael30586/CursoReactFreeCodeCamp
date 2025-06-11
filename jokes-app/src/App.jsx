e737ba8fd4169e95df6c3aa708ff62c9220a89df
Rafael305 <fraq86@gmail.com>
2025-06-11 16:17:23 -0300

    props como numeros, booleans y arrays

--- jokes-app/src/App.jsx
+++ jokes-app/src/App.jsx
@@ -4,15 +4,21 @@ import viteLogo from '/vite.svg'
 import './App.css'
 import Joke from './components/Joke'
 
+
+
 function App() {
   
   return (
     <>
-      <Joke setup="What do you call a pony with a cough?" punchline="A little horse."></Joke>
-      <Joke setup="What did one hat say to the other?" punchline="You wait here. I’ll go on a head."></Joke>
-      <Joke setup="What do you call a magic dog?" punchline="A labracadabrador."></Joke>
-      <Joke setup="What did the shark say when he ate the clownfish? " punchline="This tastes a little funny."></Joke>
-      <Joke punchline="And that’s how I ended up with a llama in my bathtub and no explanation for the insurance company."></Joke>
+      <Joke setup="What do you call a pony with a cough?" punchline="A little horse." votes={1} isPun={true} comments={[
+        { author: "Me", comment: "Lame joke"},
+        { author: "You", comment: "Cool Joke"},
+        { author: "Him", comment: "Amazing joke"}
+      ]}></Joke>
+      <Joke setup="What did one hat say to the other?" punchline="You wait here. I’ll go on a head." votes={2} isPun={true}></Joke>
+      <Joke setup="What do you call a magic dog?" punchline="A labracadabrador." votes={-1} isPun={false}></Joke>
+      <Joke setup="What did the shark say when he ate the clownfish? " punchline="This tastes a little funny." votes={0} isPun={true}></Joke>
+      <Joke punchline="And that’s how I ended up with a llama in my bathtub and no explanation for the insurance company." votes={3} isPun={false}></Joke>
     </>
   )
 }
--- jokes-app/src/components/Joke.jsx
+++ jokes-app/src/components/Joke.jsx
@@ -2,7 +2,9 @@ export default function Joke(props){
     return(
         <>
             <span className="setup">{props.setup}</span> <br />
-            <span className="punchline">{props.punchline}</span> <br /> <br />
+            <span className="punchline">{props.punchline}</span> <br /> 
+            <span>votes : {props.votes}</span> <br /> 
+            <span> is pun : {props.isPun}</span> <br /> <br />
         </>
     )
 }
