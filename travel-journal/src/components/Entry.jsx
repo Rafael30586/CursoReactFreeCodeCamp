319e5346b83f2fba7673e13e932e19cc1d11c43e
Rafael305 <fraq86@gmail.com>
2025-06-12 15:49:26 -0300

    Entry component completo

--- travel-journal/src/components/Entry.jsx
+++ travel-journal/src/components/Entry.jsx
@@ -1,24 +1,25 @@
-export default function Entry(){
+export default function Entry(props){
     return (
         <article> 
             <div>
-                <img className="paisaje" src="https://res-4.cloudinary.com/jnto/image/upload/w_750,h_750,c_fill,f_auto,fl_lossy,q_auto/v1/media/filer_public/f8/02/f802e620-ebff-4c67-824a-f7198b95b21e/mount-fuji-banner_rchkty" alt="" />
+                <img className="paisaje" src={props.img.src} alt={props.img.alt} />
             </div>
             
             <div className="card"> 
                 <div>
                     <img className="marker" src="/src/assets/marker.png" alt="marker" />
-                    <span className="country-name">Japan</span>
-                    <a className="google-maps" href="https://www.google.com/maps/place/Mount+Fuji/@35.3606421,138.7170637,15z/data=!3m1!4b1!4m6!3m5!1s0x6019629a42fdc899:0xa6a1fcc916f3a4df!8m2!3d35.3606255!4d138.7273634!16zL20vMGNrczA?entry=ttu">View on google maps</a>
+                    <span className="country-name">{props.country}</span>
+                    <a className="google-maps" href={props.googleMapsLink}>View on google maps</a>
                 </div>
-                <h2>Mount Fuji</h2>
-                <h6>12, jan 2021 - 24 jan, 2021</h6>
+                <h2>{props.title}</h2>
+                <h6>{props.dates}</h6>
                 <p>
-                    Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single
-                    most popular tourist site in Japan, for both Japanese and foreign tourists.
+                    {props.text}
                 </p>
             </div>
         </article>
 
     )
 }
+
+//https://res-4.cloudinary.com/jnto/image/upload/w_750,h_750,c_fill,f_auto,fl_lossy,q_auto/v1/media/filer_public/f8/02/f802e620-ebff-4c67-824a-f7198b95b21e/mount-fuji-banner_rchkty
