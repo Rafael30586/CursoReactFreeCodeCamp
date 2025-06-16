871f1509a711f45b82bc60394a1d8bfe2f48063c
Rafael305 <fraq86@gmail.com>
2025-06-16 17:18:48 -0300

    cheaf claude header

---
+++ chef-claude/src/components/Header.jsx
@@ -0,0 +1,9 @@
+export default function Header(){
+    return(
+        <header>
+            <img src="/src/assets/chef-claude-icon.png" alt="un chef" />
+            <h1>Chef Claude</h1>
+        </header>
+    )
+
+}
