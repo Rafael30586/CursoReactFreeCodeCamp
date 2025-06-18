af0e81c296767dffa35a7f9d813cc84e65b65de3
Rafael305 <fraq86@gmail.com>
2025-06-18 16:23:23 -0300

    introduccion a usestate

--- chef-claude/src/components/Main.jsx
+++ chef-claude/src/components/Main.jsx
@@ -1,10 +1,26 @@
 export default function Main(){
+    const ingredients = ["Chicken","Oregano","Tomatoes"]
+    const ingredientList = ingredients.map((ingredient)=>{
+        return <li key={ingredient}>{ingredient}</li>
+    })
+
+    function submitForm(event){
+        event.preventDefault()
+        const formData = new FormData(event.currentTarget)
+        const newIngredient = formData.get("ingredient")
+        ingredients.push(newIngredient)
+        console.log(ingredients)
+    }
+    
     return(
         <main>
-            <form action="" className="add-ingredient-form">
-                <input type="text" aria-label="Add ingredient" placeholder="eg: Oregano"/>
+            <form action="" className="add-ingredient-form" onSubmit={submitForm}>
+                <input type="text" aria-label="Add ingredient" placeholder="eg: Oregano" name="ingredient"/>
                 <button>Add ingredient</button>
             </form>
+            <ul>
+                {ingredientList}
+            </ul>
         </main>
     )
 }
