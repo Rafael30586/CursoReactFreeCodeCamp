c98b30ba006a4d185481f74a49bc7c16113c5380
Rafael305 <fraq86@gmail.com>
2025-06-26 16:06:06 -0300

    objeto a partir de formData

--- chef-claude/src/main.jsx
+++ chef-claude/src/main.jsx
@@ -1,12 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
+import './index8.css'
 import App from './App.jsx'
 import App8 from './App8.jsx'
 
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <App8 />
   </StrictMode>,
 )
