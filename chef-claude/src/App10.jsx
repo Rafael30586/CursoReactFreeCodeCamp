58cf96e463f7918a88d03d194978c809ae9ec2b6
Rafael305 <fraq86@gmail.com>
2025-07-03 15:53:52 -0300

    evento en botones de colores

--- chef-claude/src/App10.jsx
+++ chef-claude/src/App10.jsx
@@ -7,26 +7,11 @@ export default function App10(props) {
     const [pad,setPad] = React.useState(pads)
 
     
-    let darkColor = {
-        backgroundColor:"#222222"
-    }
-
-    let lightColor = {
-        backgroundColor:"#cccccc"
-    }
 
     const buttons = pad.map((button)=>{ // El estilo que se le pasa al botón es como objeto
-        return <Pad color={button.color}></Pad>
+        return <Pad turn={button.on} color={button.color}></Pad>
     })
-    /**
-     * Challenge part 1:
-     * 1. Initialize state with the default value of the
-     *    array pulled in from pads.js
-     * 2. Map over that state array and display each one
-     *    as a <button> (CSS is already written for you)
-     *    (Don't worry about using the "on" or "color" 
-     *    properties yet)
-     */
+    
     return (
         <main>
             <div className="pad-container">
--- chef-claude/src/components/Pad.jsx
+++ chef-claude/src/components/Pad.jsx
@@ -1,9 +1,26 @@
+import React from "react"
+
 export default function Pad(props){
+    const [on,setOn] = React.useState(props.turn)
+
     let background = {
         backgroundColor:props.color
     }
+
+    function toggleOn(){
+        if(props.on){
+            setOn(true)
+        }else{
+            setOn(false)
+        }
+    }
+
+    function toggleButton(){
+        setOn((prevOn)=>!prevOn)
+    }
+
     return (
-        <button style={background}></button>
+        <button onClick={toggleButton} className={on ? "on" : "off"} style={background}></button>
     )
 
 }
