d247b1a1f943dab54edf01c9b6a23131228b952c
Rafael305 <fraq86@gmail.com>
2025-07-23 15:57:28 -0300

    dados en objetos

--- tenzies/src/App.jsx
+++ tenzies/src/App.jsx
@@ -3,27 +3,29 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import Die from './components/Die'
 import React from 'react'
+import { nanoid } from 'nanoid'
 
 
 function App() {
 
-  const [numbers,setNumbers] = React.useState(generateAllNewDice())
+  const [dice,setDice] = React.useState(generateAllNewDice())
   // let id = 1
   function generateAllNewDice(){
-    let dice = []
+    let diceArray = []
     for(let i=0;i<10;i++){
-      dice.push((Math.floor(Math.random() * 6) + 1))
+      //dice.push((Math.floor(Math.random() * 6) + 1))
+      diceArray.push({value:(Math.floor(Math.random() * 6) + 1) ,isHeld:false, id: nanoid()})
     }
-    return dice
+    return diceArray
   }
 
-  let diceArray = numbers.map((number,index)=>{
+  let diceArray = dice.map((die,index)=>{
     let dieId = "die".concat(index) // Generación del id para el Die component que servirá después para el grid de css
-    return <Die id={dieId} value={number}></Die>
+    return <Die id={dieId} value={die.value} key={die.id}></Die>
   })
 
   function rollDice(){
-    setNumbers(()=>{
+    setDice(()=>{
       return generateAllNewDice()
     })
 
