d7a8850b9b21110a628c43303d805e99578ba0ce
Rafael305 <fraq86@gmail.com>
2025-08-07 16:15:33 -0300

    header del assembly

---
+++ assembly-endgame/src/App.jsx
@@ -0,0 +1,17 @@
+
+import AssemblyHeader from './components/AssemblyHeader'
+
+
+function App() {
+ 
+  return (
+    <>
+      <AssemblyHeader></AssemblyHeader>
+      <main>
+        game goes here
+      </main>
+    </>
+  )
+}
+
+export default App
---
+++ assembly-endgame/src/components/AssemblyHeader.jsx
@@ -0,0 +1,8 @@
+export default function AssemblyHeader(){
+    return(
+        <header>
+            <h4>Assembly: endgame</h4>
+            <p>Guess the word in under 8 attempts to keep the programming world safe from Assembly</p>
+        </header>
+    )
+}
