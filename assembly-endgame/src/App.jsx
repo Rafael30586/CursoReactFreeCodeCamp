9a82073e4170ce196ea3a2fc4c03a6542f1212df
Rafael305 <fraq86@gmail.com>
2025-08-12 15:56:15 -0300

    currentWord state

--- assembly-endgame/src/App.jsx
+++ assembly-endgame/src/App.jsx
@@ -2,9 +2,27 @@
 import AssemblyHeader from './components/AssemblyHeader'
 import Languages from './components/Languages'
 import StatusSection from './components/StatusSection'
+import React from 'react'
 
 
 function App() {
+
+  const [currentWord, setCurrentWord] = React.useState("React")
+
+  function converToArray(){
+    let wordLength = currentWord.length
+    let charArray = []
+    for(let i=0;i<wordLength;i++){
+      charArray.push(currentWord.charAt(i))
+    }
+    return charArray
+  }
+
+  function charToSpan(){
+    let charArray = converToArray()
+    let spanCharacters = charArray.map(char=><span className='char'>{char.toUpperCase()}</span>)
+    return spanCharacters
+  }
  
   return (
     <>
@@ -12,6 +30,7 @@ function App() {
       <StatusSection></StatusSection>
       <main>
         <Languages></Languages>
+        <section id='currentWord'>{charToSpan()}</section>
       </main>
     </>
   )
